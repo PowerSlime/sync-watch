bdde49f859b51cb8e0f12567ee33b35b0b6e1a77
Denis <powerslime@ya.ru>
2019-08-24 17:45:12 +0300

    Minor dependencies update

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ module.exports = {
     /*
      ** Nuxt.js dev-modules
      */
-    devModules: [
+    buildModules: [
         // Doc: https://github.com/nuxt-community/eslint-module
         "@nuxtjs/eslint-module",
         "@nuxtjs/vuetify",
